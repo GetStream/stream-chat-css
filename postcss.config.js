f240d3ce29a5d1319eefaf08c6f5d06e11c3c5b4
Zita Szupera <szuperaz@gmail.com>
2022-05-18 11:33:45 +0200

    fix: Remove flex-gap polyfill

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,7 @@
 const postcssLogical = require('postcss-logical');
 const postcssDirPseudoClass = require('postcss-dir-pseudo-class');
-const postcssFlexGapPolyfill = require('flex-gap-polyfill');
 
 module.exports = {
   syntax: 'postcss-scss',
-  plugins: [postcssLogical({ preserve: true }), postcssDirPseudoClass(), postcssFlexGapPolyfill()],
+  plugins: [postcssLogical({ preserve: true }), postcssDirPseudoClass()],
 };
