b4d1658e9d59f6428ae359204ff048bc120f082e
Zita Szupera <szuperaz@gmail.com>
2024-04-25 15:59:46 +0200

    feat(angular): allow customizing avatar, icon and loading indicator from CSS (#291)
    ### 🎯 Goal
    
    Allow customizing avatar, icon and loading indicator components from
    CSS.
    
    Corresponding GitHub issues:
    - https://github.com/GetStream/stream-chat-css/issues/84
    - https://github.com/GetStream/stream-chat-css/issues/84
    
    It's only implemented for Angular, React can follow at it's own pace
    
    ### 🛠 Implementation details
    
    This is a non-breaking change, the old CSS code remains, and can be used
    by React and Angular@4.
    
    #### Avatar
    
    The `--str-chat__avatar-size` variable allows customizing the avatar
    size
    
    ![Screenshot 2024-04-23 at 17 13
    29](https://github.com/GetStream/stream-chat-css/assets/6690098/59f6dccf-25b9-4c61-95cc-36ddc31cc66a)
    
    #### Icon
    
    ![Screenshot 2024-04-23 at 17 23
    40](https://github.com/GetStream/stream-chat-css/assets/6690098/bb9da2b0-5431-4e32-b7d5-f0f8a3392301)
    
    Why use fonts for icons? I generated a custom font from the icons we use
    in chat UI components, there is one downside that I hate: the font needs
    to be manually regenerated when we want to add a new icon (which is not
    that often). However, as far as I know fonts provide the best
    flexibility for setting size and color properties of icons. It's also
    used by big libraries such as [Google Material
    Icons](https://fonts.google.com/icons) so our integrators are probably
    familiar with the concept.
    
    As a comparison: in video the icons are also customizable from CSS, but
    we use
    [`mask-image`](https://github.com/GetStream/stream-video-js/blob/main/packages/styling/src/Icon/Icon-theme.scss#L16)
    with encoded SVGs, the downside of that solution is that we have to
    explicitly provide both `height` and `width`, which causes problems if
    an integrator wants to use an icon with a different aspect-ratio than
    the built-in icon.
    
    #### Loading indicator
    
    The `--str-chat__loading-indicator-size` allows setting avatar size from
    CSS
    
    ![Screenshot 2024-04-24 at 18 15
    00](https://github.com/GetStream/stream-chat-css/assets/6690098/2f89a466-cd53-466a-9ef6-688b11f26a23)
    
    I wasn't able to move the loading indicator SVG to CSS, I had troubles
    applying the color from the CSS variable to the stop color 🤷‍♀️
    
    For the curious, here is the Angular upgrade guide with the breaking
    changes:
    ![Screenshot 2024-04-24 at 18 23
    45](https://github.com/GetStream/stream-chat-css/assets/6690098/ae947f85-2670-48af-9023-ea2db7dcdf4a)
    
    ![Screenshot 2024-04-24 at 18 24
    03](https://github.com/GetStream/stream-chat-css/assets/6690098/ecf29085-822c-457c-960c-eefa60d8c010)
    
    ![Screenshot 2024-04-24 at 18 24
    10](https://github.com/GetStream/stream-chat-css/assets/6690098/e9a743c0-076a-4e5c-afc7-f76fb7270e68)
    
    
    
    ### 🎨 UI Changes
    
    No UI changes (hopefully 😅)
    
    Make sure to test with both Angular and React (with both `MessageList`
    and `VirtualizedMessageList` components) SDKs

--- scripts/output.ts
+++ scripts/output.ts
@@ -1,46 +1,69 @@
 import dedent from 'dedent';
 import prettier from 'prettier';
 import type { VariableGroup, VariableInfo } from './parser';
-import * as packagejson from '../package.json'
+import * as packagejson from '../package.json';
 
-type Column = {name: string, key: keyof VariableInfo | 'usedIn', type: 'code' | 'text' | 'values'};
-type Group = {name: string, regexp: RegExp, columns: Column[], definedIn?: Function};
+type Column = {
+  name: string;
+  key: keyof VariableInfo | 'usedIn';
+  type: 'code' | 'text' | 'values';
+};
+type Group = { name: string; regexp: RegExp; columns: Column[]; definedIn?: Function };
 
 const row = (v: VariableInfo, group: Group) => {
   const usedIn = [...v.referencedIn].map(componentThemeLink).join(', ');
-  const info = {...v, usedIn};
-  return dedent`${group.columns.map(c => `| ${getColumn(c, info)}`).join('')}|`
+  const info = { ...v, usedIn };
+  return dedent`${group.columns.map((c) => `| ${getColumn(c, info)}`).join('')}|`;
 };
 
-const getColumn = (column: Column, info: VariableInfo & {usedIn?: string}) => {
+const getColumn = (column: Column, info: VariableInfo & { usedIn?: string }) => {
   if (column.type === 'values') {
     return getValuesColumn(info);
   } else {
     return getTextOrCodeColumn(column, info);
   }
-}
+};
 
-const getValuesColumn = (info: VariableInfo & {usedIn?: string}) => {
-  return `<table>${info.values.map(v => `<tr><th>\`${v.scope}\`</th></tr><tr><td>\`${v.value}\`</td></tr>`).join('')}</table>`;
-}
+const getValuesColumn = (info: VariableInfo & { usedIn?: string }) => {
+  return `<table>${info.values
+    .map((v) => `<tr><th>\`${v.scope}\`</th></tr><tr><td>\`${v.value}\`</td></tr>`)
+    .join('')}</table>`;
+};
 
-const getTextOrCodeColumn = (column: Column, info: VariableInfo & {usedIn?: string}) => {
-  return `${column.type === 'code' ? '\`' : ''}${info[column.key] || ''}${column.type === 'code' ? '\`' : ''}`
-}
+const getTextOrCodeColumn = (column: Column, info: VariableInfo & { usedIn?: string }) => {
+  return `${column.type === 'code' ? '`' : ''}${info[column.key] || ''}${
+    column.type === 'code' ? '`' : ''
+  }`;
+};
 
-export const getGlobalVariablesOutput = (data: Map<string, VariableInfo>, type: 'theme' | 'layout') => {
-  const nameColumn: Column = {name: 'Name', key: 'name', type: 'code'};
-  const valueColumn: Column = {name: 'Value(s)', key: 'values', type: 'values'};
-  const descriptionColumn: Column = {name: 'Description', key: 'description', type: 'text'};
-  const usedInColumn: Column = {name: 'Used in', key: 'usedIn', type: 'text'}
-  
+export const getGlobalVariablesOutput = (
+  data: Map<string, VariableInfo>,
+  type: 'theme' | 'layout',
+) => {
+  const nameColumn: Column = { name: 'Name', key: 'name', type: 'code' };
+  const valueColumn: Column = { name: 'Value(s)', key: 'values', type: 'values' };
+  const descriptionColumn: Column = { name: 'Description', key: 'description', type: 'text' };
+  const usedInColumn: Column = { name: 'Used in', key: 'usedIn', type: 'text' };
+
   const groups = [
-    {name: 'Colors', regexp: /color/, columns: [nameColumn, valueColumn, descriptionColumn, usedInColumn]},
-    {name: 'Typography', regexp: /(__font|-text)/, columns: [nameColumn, valueColumn, descriptionColumn, usedInColumn]},
-    {name: 'Spacing', regexp: /spacing/, columns: [nameColumn, valueColumn, descriptionColumn]},
-    {name: 'Radius', regexp: /__border-radius/, columns: [nameColumn, valueColumn, descriptionColumn, usedInColumn]},
-    {name: 'Others', regexp: /.*/, columns: [nameColumn, valueColumn, descriptionColumn]}
-  ]
+    {
+      name: 'Colors',
+      regexp: /color/,
+      columns: [nameColumn, valueColumn, descriptionColumn, usedInColumn],
+    },
+    {
+      name: 'Typography',
+      regexp: /(__font|-text)/,
+      columns: [nameColumn, valueColumn, descriptionColumn, usedInColumn],
+    },
+    { name: 'Spacing', regexp: /spacing/, columns: [nameColumn, valueColumn, descriptionColumn] },
+    {
+      name: 'Radius',
+      regexp: /__border-radius/,
+      columns: [nameColumn, valueColumn, descriptionColumn, usedInColumn],
+    },
+    { name: 'Others', regexp: /.*/, columns: [nameColumn, valueColumn, descriptionColumn] },
+  ];
 
   const variablesByGroups: Map<Group, string[]> = new Map();
 
@@ -57,9 +80,13 @@ export const getGlobalVariablesOutput = (data: Map<string, VariableInfo>, type: 
   });
 
   let output = '';
-  groups.forEach(group => {
+  groups.forEach((group) => {
     if (variablesByGroups.get(group)) {
-      const header = group.columns.map(c => `| ${c.name}`).join('') + `| \n` + group.columns.map(() => '|-').join('') + `|`;
+      const header =
+        group.columns.map((c) => `| ${c.name}`).join('') +
+        `| \n` +
+        group.columns.map(() => '|-').join('') +
+        `|`;
       output += dedent`
         ### ${group.name}
         ${header}
@@ -67,32 +94,44 @@ export const getGlobalVariablesOutput = (data: Map<string, VariableInfo>, type: 
     }
   });
 
-  output += `All global ${type} variables are defined in: [https://github.com/GetStream/stream-chat-css/tree/v${packagejson.version}/src/v2/styles/_global-${type}-variables.scss](https://github.com/GetStream/stream-chat-css/tree/v${packagejson.version}/src/v2/styles/_global-${type}-variables.scss)\n\n`
+  output += `All global ${type} variables are defined in: [https://github.com/GetStream/stream-chat-css/tree/v${packagejson.version}/src/v2/styles/_global-${type}-variables.scss](https://github.com/GetStream/stream-chat-css/tree/v${packagejson.version}/src/v2/styles/_global-${type}-variables.scss)\n\n`;
 
   return format(output);
 };
 
 export const getComponentVariablesOutput = (data: Map<string, VariableInfo>) => {
-  const nameColumn: Column = {name: 'Name', key: 'name', type: 'code'};
-  const valueColumn: Column = {name: 'Value(s)', key: 'values', type: 'values'};
-  const descriptionColumn: Column = {name: 'Description', key: 'description', type: 'text'};
+  const nameColumn: Column = { name: 'Name', key: 'name', type: 'code' };
+  const valueColumn: Column = { name: 'Value(s)', key: 'values', type: 'values' };
+  const descriptionColumn: Column = { name: 'Description', key: 'description', type: 'text' };
 
   const subgroupDefinitions = [
-    {name: 'Theme variables', regexp: /color|border|box-shadow|overlay|background/, columns: [nameColumn, valueColumn, descriptionColumn], definedIn: componentThemeLink},
-    {name: 'Layout variables', regexp: /.*/, columns: [nameColumn, valueColumn, descriptionColumn], definedIn: componentLayoutLink},
+    {
+      name: 'Theme variables',
+      regexp: /color|border|box-shadow|overlay|background/,
+      columns: [nameColumn, valueColumn, descriptionColumn],
+      definedIn: componentThemeLink,
+    },
+    {
+      name: 'Layout variables',
+      regexp: /.*/,
+      columns: [nameColumn, valueColumn, descriptionColumn],
+      definedIn: componentLayoutLink,
+    },
   ];
 
   const componentsGroups: Map<VariableGroup, Map<Group, string[]>> = new Map();
   data.forEach((v) => {
     const variableGroup = v.definedIn;
     if (variableGroup) {
-      let existingVariableGroup = Array.from(componentsGroups.keys()).find(g => g.componentName === variableGroup.componentName);
+      let existingVariableGroup = Array.from(componentsGroups.keys()).find(
+        (g) => g.componentName === variableGroup.componentName,
+      );
       if (!existingVariableGroup) {
         componentsGroups.set(variableGroup, new Map());
         existingVariableGroup = variableGroup;
       }
       const componentGroup = componentsGroups.get(existingVariableGroup)!;
-      const subgroup = subgroupDefinitions.find(subgroup => subgroup.regexp.test(v.name));
+      const subgroup = subgroupDefinitions.find((subgroup) => subgroup.regexp.test(v.name));
       if (subgroup) {
         if (!componentGroup.get(subgroup)) {
           componentGroup.set(subgroup, []);
@@ -103,21 +142,31 @@ export const getComponentVariablesOutput = (data: Map<string, VariableInfo>) => 
   });
 
   let output = '';
-  Array.from(componentsGroups.entries()).forEach(([variableGroup, variablesBySubgroups]) => {
-    output += dedent`
-      ## ${variableGroup.componentName}${variableGroup.sdkRestriction ? ` - Only available in ${variableGroup.sdkRestriction} SDK` : ''}\n`;
-    
-      subgroupDefinitions.forEach(group => {
-      if (variablesBySubgroups.get(group)) {
-        const header = group.columns.map(c => `| ${c.name}`).join('') + `| \n` + group.columns.map(() => '|-').join('') + `|`;
-        output += dedent`
+  Array.from(componentsGroups.entries())
+    .sort((e1, e2) => e1[0].componentName.localeCompare(e2[0].componentName))
+    .forEach(([variableGroup, variablesBySubgroups]) => {
+      output += dedent`
+      ## ${variableGroup.componentName}${
+        variableGroup.sdkRestriction
+          ? ` - Only available in ${variableGroup.sdkRestriction} SDK`
+          : ''
+      }\n`;
+
+      subgroupDefinitions.forEach((group) => {
+        if (variablesBySubgroups.get(group)) {
+          const header =
+            group.columns.map((c) => `| ${c.name}`).join('') +
+            `| \n` +
+            group.columns.map(() => '|-').join('') +
+            `|`;
+          output += dedent`
           ### ${group.name}
           ${header}
           ${variablesBySubgroups.get(group)!.join('\n')}\n
           Defined in: ${group.definedIn(variableGroup.componentName)}\n\n`;
-      }
+        }
+      });
     });
-  });
 
   return format(output);
 };
